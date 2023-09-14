96475e667907cb290f636a28ca1a17ca0838cb7f
Yura Pokrovsky <y-pokrovsky@yandex.ru>
2023-09-14 12:20:16 +0300

    order service interacts with inventory service via webflux

--- inventory-service/src/main/java/com/micro/services/example/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/micro/services/example/inventoryservice/controller/InventoryController.java
@@ -1,9 +1,12 @@
 package com.micro.services.example.inventoryservice.controller;
 
+import com.micro.services.example.inventoryservice.dto.InventoryResponse;
 import com.micro.services.example.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -12,9 +15,10 @@ public class InventoryController {
 
     private final InventoryService inventoryService;
 
-    @GetMapping("/{sku-code}")
+    // изменим аргумент на параметры запроса
+    @GetMapping()
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
         return inventoryService.isInStock(skuCode);
     }
 }
--- inventory-service/src/main/java/com/micro/services/example/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/micro/services/example/inventoryservice/service/InventoryService.java
@@ -1,9 +1,14 @@
 package com.micro.services.example.inventoryservice.service;
 
+import com.micro.services.example.inventoryservice.dto.InventoryResponse;
 import com.micro.services.example.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @Service
 @RequiredArgsConstructor
@@ -12,7 +17,14 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode) {
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCode) {
+        return inventoryRepository.findBySkuCodeIn(skuCode)
+                .stream()
+                .map(inventory ->
+                    InventoryResponse.builder()
+                            .skuCode(inventory.getSkuCode())
+                            .isInStock(inventory.getQuantity() > 0)
+                            .build()
+                ).toList();
     }
 }

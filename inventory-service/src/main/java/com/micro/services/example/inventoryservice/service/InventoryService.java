c2ff377fcd842424ecf293c59fa020c0fddab739
Yura Pokrovsky <y-pokrovsky@yandex.ru>
2023-09-15 14:22:17 +0300

    implementing Resilience4j patterns

--- inventory-service/src/main/java/com/micro/services/example/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/micro/services/example/inventoryservice/service/InventoryService.java
@@ -3,6 +3,8 @@ package com.micro.services.example.inventoryservice.service;
 import com.micro.services.example.inventoryservice.dto.InventoryResponse;
 import com.micro.services.example.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,12 +14,17 @@ import static java.util.stream.Collectors.toList;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
+    @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> skuCode) {
+        log.info("включается задержка");
+        Thread.sleep(10000);
+        log.info("задержка закончилась");
         return inventoryRepository.findBySkuCodeIn(skuCode)
                 .stream()
                 .map(inventory ->
